8ea35a243007532da67ebf3c2a3b7095e29b013f
dirFY-24hosodarikuto <rikuto.hosoda.b926@dirbato.co.jp>
2024-04-24 09:14:14 +0900

    EncodingFilter作成

---
+++ daily_report_system2/src/main/java/filters/Encodingfilter.java
@@ -0,0 +1,55 @@
+package filters;
+
+import java.io.IOException; 
+
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ *  Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class Encodingfilter {
+
+	
+	/** 
+	 * Default constructor.
+	 */
+	public Encodingfilter() {
+		
+		
+	}
+	
+	/**
+	 *  @see Filter#destroy
+	 */
+	public void destroy() {	
+		
+	}
+
+	/**
+	 * @see Filter#doFiter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		
+		chain.doFilter(request, response);
+		
+	}
+
+	/**
+	 * @see Filter"init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+}
+	
+}
